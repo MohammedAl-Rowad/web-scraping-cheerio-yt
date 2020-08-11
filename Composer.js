6afccc30a4f4fa3e183a848103f94a07eb5b8b00
MohammedAl-rowad <rowadwo@gmail.com>
2020-08-11 18:30:03 +0300

    get the dom as json recursively

---
+++ Composer.js
@@ -0,0 +1,33 @@
+module.exports = class DOMScrapperComposer {
+  constructor($) {
+    this.$ = $
+  }
+
+  domToJSON() {
+    const mainParents = ['head', 'body']
+    return mainParents.map((name) => ({
+      name,
+      childrenCount: this.childrenArr(name).length,
+      children: this.getChildData(this.childrenArr(name)),
+    }))
+  }
+
+  childrenArr(el) {
+    return this.$(el).children().toArray()
+  }
+
+  getChildData(ce) {
+    return ce.map((el) => {
+      const len = this.$(el).children().length
+      const nodeInfo = {
+        name: this.$(el).prop('tagName'),
+        value: len,
+      }
+      if (len >= 1) {
+        nodeInfo.children = [...this.getChildData(this.childrenArr(el))]
+      }
+
+      return nodeInfo
+    })
+  }
+}
--- index.js
+++ index.js
@@ -2,13 +2,20 @@ const { load } = require('cheerio')
 const fetch = require('node-fetch')
 const { writeFileSync } = require('fs')
 const { generate } = require('shortid')
+const DOMScrapperComposer = require('./Composer')
 
 const init = async () => {
-  const url = 'https://www.infinitysymbol.net'
+  const url =
+    'https://github.com/MohammedAl-Rowad/Yedua/blob/code/Yedua-api/src/composers/DomScraper.ts'
   const htmlStr = await (await fetch(url)).text()
   const $ = load(htmlStr)
-  console.log($.html())
   writeFileSync(`./htmls/${generate()}-${Date.now()}.html`, $.html())
+  const composer = new DOMScrapperComposer($)
+  const json = composer.domToJSON()
+  writeFileSync(
+    `./jsons/${generate()}-${Date.now()}.json`,
+    JSON.stringify(json, null, 2)
+  )
 }
 
 init()
